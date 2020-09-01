7cfd845d04a0a0aa956522fbcb65b0732b9c6658
ole kristian homelien <ole_kristian92@hotmail.com>
2020-09-01 22:50:45 +0200

    Change

---
+++ VariantMeshEditor/Util/VariantMeshLoader.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VariantMeshEditor.Util
+{
+    class VariantMeshLoader
+    {
+
+        public void Load(string meshfile)
+        {
+            // Load the variant file
+                // Load each model in the variant file
+                // Load each file
+                // Ensure all files have the same skeleton
+
+            // Load the skeleton
+            // Load all animations for this model
+
+            // Load all textures
+
+            // Load all texture vsmodel files?
+                // Load all textures in them
+
+            {   // Precalc and create a file
+
+                // Find all models for this skeleton
+                // Find all veraint meshes and build map from model to variant mesh slot type
+            }
+
+            // IF error return something clever. If not same skeleton, dont animate.
+            // If not found, dont display => show error
+
+
+        }
+    }
+}
